8a2fd986569cc977a6c11e6e213e39c90390ee95
Dilki <dilkihansapani321@gmail.com>
2024-12-21 23:46:56 +0530

    frontend:add basic components

--- frontend/stock_management_system_frontend/src/App.js
+++ frontend/stock_management_system_frontend/src/App.js
@@ -1,8 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import Header from "./Components/Header/Header";
+import Sidebar from "./Components/SideMenu/Sidebar";
+import Footer from "./Components/Footer/Footer";
+
+import Sellers from "./Components/PageContent/Sellers";
+import Products from "./Components/PageContent/Products";
+import Inventories from "./Components/PageContent/Inventories";
+import Items from "./Components/PageContent/Items";
+import Orders from "./Components/PageContent/Orders";
+import Reports from "./Components/PageContent/Reports";
 
 function App() {
-  return <div className="App">hello</div>;
+  const [selectedContent, setSelectedContent] = useState("Manage Sellers");
+
+  const renderContent = () => {
+    switch (selectedContent) {
+      case "Sellers":
+        return <Sellers />;
+      case "Products":
+        return <Products />;
+      case "Inventories":
+        return <Inventories />;
+      case "Items":
+        return <Items />;
+      case "Orders":
+        return <Orders />;
+      case "Reports":
+        return <Reports />;
+      default:
+        return <h1>Welcome to Inventory Management</h1>;
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <Header />
+      <div style={{ display: "flex", flex: 1 }}>
+        <Sidebar onMenuSelect={(item) => setSelectedContent(item)} />
+        <div style={{ flex: 1, padding: "20px", overflowY: "auto" }}>
+          {renderContent()}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/stock_management_system_frontend/src/Components/PageContent/Products.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Products = () => {
+  return <div>Manage Product Component</div>;
+};
+
+export default Products;
